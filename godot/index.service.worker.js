c5dd4015e1342947fd8017870f44b550c3fd1f90
aeiouvcode <aievoli.x@gmail.com>
2026-09-23 22:03:53 +0530

    OUTWARD Godot c4: fix twist subtitle clipping on phones

--- godot/index.service.worker.js
+++ godot/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790180551|4017248';
+const CACHE_VERSION = '1790181215|4043512';
 /** @type {string} */
 const CACHE_PREFIX = 'OUTWARD-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
