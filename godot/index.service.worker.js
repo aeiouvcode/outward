c4bf95625e206f916cac1e653982f3215e1918c3
aeiouvcode <aievoli.x@gmail.com>
2026-09-23 22:01:26 +0530

    OUTWARD Godot c4: snake-model rebuild + level twists, board parity with web

--- godot/index.service.worker.js
+++ godot/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790157139|4225051';
+const CACHE_VERSION = '1790180551|4017248';
 /** @type {string} */
 const CACHE_PREFIX = 'OUTWARD-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
